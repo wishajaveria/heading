55307a4ae001d8f63839fe61e31226cdcda23c49
wishajaveria <49516832+wishajaveria@users.noreply.github.com>
2019-06-30 19:06:04 +0500

    wip

---
+++ script.js
@@ -0,0 +1,3 @@
+<input type="button" value="button name" onclick="window.open('http://A paragraph is a series of sentences that are organized and coherent, and are all related to a single topic. Almost every piece of writing you do that is longer than a few sentences should be organized into paragraphs..com)" />
+
+
